751613b972f23dada8883d41d25766dbe206e4ba
Benito Palacios Sánchez <benito356@gmail.com>
2014-06-02 01:04:52 +0200

    Añadido scripts para ejecutar encapsuladocam desde RPI

---
+++ Tests/EncapsuladoCam/run_client.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+LD_LIBRARY_PATH=/home/pi/Desktop/libre/lib/armv6vfphLinux3.xgcc4.7.2jdk /home/pi/apps/jdk1.8.0/jre/bin/java -cp /home/pi/locaviewer/Libs/JNA/jna-4.1.0.jar:/home/pi/locaviewer/Libs/JNA/jna-platform-4.1.0.jar:/home/pi/locaviewer/Libs/vlcj/vlcj-2.4.1.jar:/home/pi/Desktop/libre/class/nddsjavad.jar:/home/pi/locaviewer/Tests/EncapsuladoCam/dist/EncapsuladoCam.jar encapsuladocam.Cliente
