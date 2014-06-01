768f838692ebd7759176b2dcf1519272946448bd
root <root@nitehack.(none)>
2014-06-01 11:15:11 +0200

    Actualizacion del codigo de obtencion

--- Localizacion/Obtencion/sensor.py
+++ Localizacion/Obtencion/sensor.py
@@ -6,34 +6,35 @@ import sys
 
 #MAC del sensor
 MAC_blue=sys.argv[1]
+Port=sys.argv[2]
 
 #Averiguamos su ID
 ID_blue=bluez.hci_get_route(MAC_blue)
 
-DUTYCICLES = 2
+#DUTYCICLES = 2
 
 def inquiry(inquirier):
     inquirier.inquiry()
     while inquirier.is_inquiring():
         inquirier.process_event()
     
-    return inquirier.get_mean()
+    return 
 
-def inquiry_times(inquirier, times):
-    inquirier.clear_samples()
-    for i in range(times):
-        inquiry(inquirier)
-    return inquirier.get_mean()
+#def inquiry_times(inquirier, times):
+#    inquirier.clear_samples()
+#    for i in range(times):
+#        inquiry(inquirier)
+#    return inquirier.get_mean()
 
 
     
-inquirier = BluezInquiry(ID_blue)
+inquirier = BluezInquiry(ID_blue,Port)
 
 
 
-while True:    
-    RSSI = inquiry_times(inquirier, DUTYCICLES)
-    #envio
+while True: 
+	inquiry(inquirier)
+
     
             
 
