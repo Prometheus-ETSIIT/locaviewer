b93146fd03cfdf470db6e1f4184f437eaf09f73b
Benito Palacios Sánchez <benito356@gmail.com>
2014-09-19 00:21:44 +0200

    Gava: Mejorado scripts (aceptan parámetros para los programas) Cada uno tiene que actualizarlos a sus rutas de archivos

--- Tests/Gava/run_suscriptor.sh
+++ Tests/Gava/run_suscriptor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd build/classes
 export GST_DEBUG_DUMP_DOT_DIR=$HOME
-LD_LIBRARY_PATH=$NDDSHOME/lib/x64Linux2.6gcc4.4.5jdk java -cp ../../../../Libs/gstreamer-java/gstreamer-java-1.6.jar:../../../../Libs/JNA/jna-4.1.0.jar:../../../../Libs/JNA/jna-platform-4.1.0.jar:$NDDSHOME/class/nddsjavad.jar:./ gava.Suscriptor
+source /home/benito/Programas/RTI/rti_set_bash_5.1.0
+LD_LIBRARY_PATH=$NDDSHOME/lib/x64Linux2.6gcc4.4.5jdk java -cp ../../../../Libs/gstreamer-java/gstreamer-java-1.6.jar:../../../../Libs/JNA/jna-4.1.0.jar:../../../../Libs/JNA/jna-platform-4.1.0.jar:$NDDSHOME/class/nddsjavad.jar:./ gava.Suscriptor $@
