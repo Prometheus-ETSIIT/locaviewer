5b4d66f35a7cbf21ed2edb11b37bb2b3bd5d5ff3
Benito Palacios Sánchez <benito356@gmail.com>
2014-09-19 01:08:09 +0200

    Comunicador: Scripts para ejecución

---
+++ ComunicadorArreglo/run_sensor.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd bin
+source /home/benito/Programas/RTI/rti_set_bash_5.1.0
+sudo LD_LIBRARY_PATH=$NDDSHOME/lib/x64Linux2.6gcc4.4.5jdk java -cp ../../Libs/JavaOctave/javaoctave-0.6.4.jar:../../Libs/JavaOctave/commons-logging-1.1.3.jar:$NDDSHOME/class/nddsjavad.jar:./ comunicador.Sensor
