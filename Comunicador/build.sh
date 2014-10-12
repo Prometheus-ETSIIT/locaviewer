507f17dc54ee37e91e74c075d0bfa8d18b9cc93b
Benito Palacios Sánchez <benito356@gmail.com>
2014-10-12 23:21:22 +0200

    Comunicador: Convertido a NetBeans

--- Comunicador/build.sh
+++ Comunicador/build.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-cd src/comunicador/
-
-source $RTI_CONNEXT_PATH/rti_set_bash_5.1.0
-javac -cp ../../../Libs/JavaOctave/javaoctave-0.6.4.jar:../../../Libs/JavaOctave/commons-logging-1.1.3.jar:$NDDSHOME/class/nddsjavad.jar -encoding ISO-8859-1 *.java
-mv *.class ../../bin/comunicador/
+ant -f ./build.xml -Dnb.internal.action.name=rebuild clean jar
