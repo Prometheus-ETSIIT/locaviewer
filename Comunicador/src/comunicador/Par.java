b7c246723e6d5532735734bdfbcf985f26557102
Benito Palacios Sánchez <benito356@gmail.com>
2014-10-13 01:05:52 +0200

    Comunicador: Formateo de Par para JavaDocs

--- Comunicador/src/comunicador/Par.java
+++ Comunicador/src/comunicador/Par.java
@@ -1,28 +1,77 @@
+/*
+ * Copyright (C) 2014 Prometheus
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
 package comunicador;
 
-public class Par<tipoPrimero, tipoSegundo> {
-    private tipoPrimero primero;
-    private tipoSegundo segundo;
+/**
+ * Par de datos.
+ * 
+ * @param <T> Tipo del primer dato.
+ * @param <E> Tipo del segundo dato.
+ */
+public class Par<T, E> {
+    private T primero;
+    private E segundo;
 
-    
-    public Par(tipoPrimero nuevoPrimero, tipoSegundo nuevoSegundo) {
+    /**
+     * Crea una nueva instancia de la clase.
+     * 
+     * @param nuevoPrimero Primer dato.
+     * @param nuevoSegundo Segundo dato.
+     */
+    public Par(T nuevoPrimero, E nuevoSegundo) {
         this.primero = nuevoPrimero;
         this.segundo = nuevoSegundo;
     }
 
-    public void setPrimero(tipoPrimero nuevoPrimero) {
+    /**
+     * Establece el primer dato.
+     * 
+     * @param nuevoPrimero Primer dato.
+     */
+    public void setPrimero(T nuevoPrimero) {
         this.primero = nuevoPrimero;
     }
 
-    public void setSecond(tipoSegundo nuevoSegundo) {
+    /**
+     * Establece el segundo dato.
+     * 
+     * @param nuevoSegundo Segundo dato.
+     */
+    public void setSecond(E nuevoSegundo) {
         this.segundo = nuevoSegundo;
     }
 
-    public tipoPrimero getPrimero() {
+    /**
+     * Obtiene el primer dato.
+     * 
+     * @return Primer dato.
+     */
+    public T getPrimero() {
         return primero;
     }
 
-    public tipoSegundo getSegundo() {
+    /**
+     * Obtiene el segundo dato.
+     * 
+     * @return Segundo dato.
+     */
+    public E getSegundo() {
         return segundo;
     }
     
