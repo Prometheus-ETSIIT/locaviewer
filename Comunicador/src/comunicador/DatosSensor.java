945457b159bc1999872050bb2498fb822f580c5c
Benito Palacios Sánchez <benito356@gmail.com>
2014-10-13 02:39:04 +0200

    Comunicador: Añadido campo sala para poder filtrar en el tópico.

--- Comunicador/src/comunicador/DatosSensor.java
+++ Comunicador/src/comunicador/DatosSensor.java
@@ -30,6 +30,9 @@ class DatosSensor {
     /** ID del escritor del dato. */
     private String id;
 
+    /** Nombre de la sala donde está el sensor. */
+    private String sala;
+    
     /** Posición del sensor Bluetooth. */
     private Par<Double, Double> sensorPos;
 
@@ -52,14 +55,16 @@ class DatosSensor {
      * Constructor para la clase DatosSensor.
      *
      * @param id ID del sensor emisor
+     * @param sala Nombre de la sala
      * @param posX Posición X del sensor
      * @param posY Posición Y del sensor
      * @param ninoId ID del niño
      * @param intensidad Intensidad de señal
      */
-    public DatosSensor(String id, double posX, double posY, String ninoId,
-            int intensidad) {
+    public DatosSensor(String id, String sala, double posX, double posY,
+            String ninoId, int intensidad) {
         this.id         = id;
+        this.sala       = sala;
         this.sensorPos  = new Par<>(posX, posY);
         this.ninoId     = ninoId;
         this.intensidad = intensidad;
@@ -70,40 +75,46 @@ class DatosSensor {
      * Constructor para la clase DatosSensor.
      *
      * @param id ID del sensor emisor
+     * @param sala Nombre de la sala
      * @param sensorPos Posición del sensor
      * @param IDnino ID del niño
      * @param intensidad Intensidad de la señal
      */
-    public DatosSensor(String id, Par<Double, Double> sensorPos, String ninoId,
-            int intensidad) {
+    public DatosSensor(String id, String sala, Par<Double, Double> sensorPos,
+            String ninoId, int intensidad) {
         this.id         = id;
+        this.sala       = sala;
         this.sensorPos  = sensorPos;
         this.ninoId     = ninoId;
         this.intensidad = intensidad;
         this.creacion   = new Date().getTime();
     }
 
-    /** Constructor para la clase DatosSensor.
+    /**
+     * Constructor para la clase DatosSensor.
      * 
      * @param mensaje String con los datos de la clase.
      */
     public DatosSensor(String mensaje) {
         String[] fields = mensaje.split(" ");
-        this.id  = fields[0];
-        double posX = Double.parseDouble(fields[1]);
-        double posY = Double.parseDouble(fields[2]);
+        this.id   = fields[0];
+        this.sala = fields[1];
+        double posX = Double.parseDouble(fields[2]);
+        double posY = Double.parseDouble(fields[3]);
         this.sensorPos  = new Par<>(posX, posY);
-        this.ninoId     = fields[3];
-        this.intensidad = Integer.parseInt(fields[4]);
-        this.creacion   = Long.parseLong(fields[5]);
+        this.ninoId     = fields[4];
+        this.intensidad = Integer.parseInt(fields[5]);
+        this.creacion   = Long.parseLong(fields[6]);
     }
     
-    /** Constructor para la clase DatosSensor.
+    /**
+     * Constructor para la clase DatosSensor.
      * 
      * @param mensaje String con los datos de la clase.
      */
     public DatosSensor(DatosSensor base, String ninoId, int intensidad) {
         this.id         = base.getID();
+        this.sala       = base.getSala();
         this.sensorPos  = base.getPosicionSensor();
         this.ninoId     = ninoId;
         this.intensidad = intensidad;
@@ -118,13 +129,14 @@ class DatosSensor {
      */
     public static DatosSensor FromDds(final DynamicData sample) {
         DatosSensor datos = new DatosSensor();
-        datos.id    = sample.get_string("id", DynamicData.MEMBER_ID_UNSPECIFIED);
+        datos.id    = sample.get_string("id",   DynamicData.MEMBER_ID_UNSPECIFIED);
+        datos.sala  = sample.get_string("sala", DynamicData.MEMBER_ID_UNSPECIFIED);
         double posX = sample.get_double("posX", DynamicData.MEMBER_ID_UNSPECIFIED);
         double posY = sample.get_double("posY", DynamicData.MEMBER_ID_UNSPECIFIED);
         datos.sensorPos  = new Par<>(posX, posY);
         datos.ninoId     = sample.get_string("ninoId", DynamicData.MEMBER_ID_UNSPECIFIED);
-        datos.intensidad = sample.get_int("rssi", DynamicData.MEMBER_ID_UNSPECIFIED);
-        datos.creacion   = sample.get_long("fecha", DynamicData.MEMBER_ID_UNSPECIFIED);
+        datos.intensidad = sample.get_int("rssi",      DynamicData.MEMBER_ID_UNSPECIFIED);
+        datos.creacion   = sample.get_long("fecha",    DynamicData.MEMBER_ID_UNSPECIFIED);
         return datos;
     }
     
@@ -137,7 +149,8 @@ class DatosSensor {
      */
     public static DatosSensor FromXml(final Element el) {
         DatosSensor datos = new DatosSensor();
-        datos.id = GetXmlEntryValue(el, "id");
+        datos.id   = GetXmlEntryValue(el, "id");
+        datos.sala = GetXmlEntryValue(el, "sala");
         double posX = Double.parseDouble(GetXmlEntryValue(el, "posX"));
         double posY = Double.parseDouble(GetXmlEntryValue(el, "posY"));
         datos.sensorPos = new Par<>(posX, posY);
@@ -155,15 +168,26 @@ class DatosSensor {
         return el.getElementsByTagName(name).item(0).getTextContent();
     }
 
-    /** Devuelve la ID del sensor emisor.
+    /**
+     * Devuelve la ID del sensor emisor.
      * 
      * @return	ID del sensor emisor.
      */
     public String getID() {
         return id;
     }
+    
+    /**
+     * Devuelve el nombre de la sala en la que está.
+     * 
+     * @return Nombre de la sala.
+     */
+    public String getSala() {
+        return sala;
+    }
 
-    /** Devuelve la posición del sensor.
+    /**
+     * Devuelve la posición del sensor.
      * 
      * @return	Posición del sensor.
      */
@@ -171,7 +195,8 @@ class DatosSensor {
         return sensorPos;
     }
 
-    /** Devuelve intensidad de señal.
+    /**
+     * Devuelve intensidad de señal.
      * 
      * @return	Intensidad de señal.
      */
@@ -179,7 +204,8 @@ class DatosSensor {
         return intensidad;
     }
 
-    /** Devuelve la ID del niño.
+    /**
+     * Devuelve la ID del niño.
      * 
      * @return	ID del niño.
      */
@@ -187,7 +213,8 @@ class DatosSensor {
         return ninoId;
     }
 
-    /** Devuelve el momento en el que fue creado el dato.
+    /**
+     * Devuelve el momento en el que fue creado el dato.
      * 
      * @return	Hora del dato.
      */
@@ -195,14 +222,16 @@ class DatosSensor {
         return creacion;
     }
 
-    /** Método toString sobrecargado.
+    /**
+     * Método toString sobrecargado.
      * 
      * @return Cadena de caracteres con el valor de los atributos.
      */
     @Override
     public String toString() {
-        return id + " " + sensorPos.getPrimero() + " " + sensorPos.getSegundo() 
-                + " " + ninoId + " " + intensidad + " " + creacion;
+        return id + " " + sala + " " + sensorPos.getPrimero() + " " +
+                sensorPos.getSegundo()  + " " + ninoId + " " + intensidad + " "
+                + creacion;
     }
     
     /**
@@ -214,6 +243,7 @@ class DatosSensor {
         datos.clear_all_members();
         
         datos.set_string("id",     DynamicData.MEMBER_ID_UNSPECIFIED, this.id);
+        datos.set_string("sala",   DynamicData.MEMBER_ID_UNSPECIFIED, this.sala);
         datos.set_double("posX",   DynamicData.MEMBER_ID_UNSPECIFIED, this.sensorPos.getPrimero());
         datos.set_double("posY",   DynamicData.MEMBER_ID_UNSPECIFIED, this.sensorPos.getSegundo());
         datos.set_string("ninoId", DynamicData.MEMBER_ID_UNSPECIFIED, this.ninoId);
