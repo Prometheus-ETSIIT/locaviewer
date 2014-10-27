96029ec41953f8523cb8270ebb5114de83dfa392
Benito Palacios Sánchez <benito356@gmail.com>
2014-10-27 16:54:39 +0100

    Config router: Añadida ruta de subred

--- config_router.sh
+++ config_router.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+# El primer argumento es la IP de la interfaz que tiene conexión a Internet
+
+# Crea la ruta hacia la subred
+sudo route add -net 192.168.3.0/24 gw 10.42.0.29 p35p1
+
 # Habilita SNAT para la segunda subred
-sudo iptables -t nat -I POSTROUTING -s 192.168.3.0/24 -o wlp4s0 -j SNAT --to 192.168.1.106
+sudo iptables -t nat -I POSTROUTING -s 192.168.3.0/24 -o wlp4s0 -j SNAT --to $1
 
 # Permite los paquetes de destino la segunda subred
 sudo iptables -t filter -I FORWARD -d 192.168.3.0/24 -j ACCEPT
