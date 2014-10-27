8d4dc57e82049d5567f77d61328a210557d1884a
Israel Blancas <iblancasa@gmail.com>
2014-10-27 17:43:16 +0100

    Actualizado el tipo de dato y añadido el apodo

--- Tests/MySQLJava/locaviewer.sql
+++ Tests/MySQLJava/locaviewer.sql
@@ -20,19 +20,15 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE IF NOT EXISTS `padres` (
-  `nino` int(6) NOT NULL,
+  `nino` varchar(200) NOT NULL,
   `padre` char(9) NOT NULL,
   `key` varchar(17) DEFAULT NULL,
   `pass` varchar(60) NOT NULL,
+  `apodo` varchar(100) NOT NULL,
   PRIMARY KEY (`nino`,`padre`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Volcado de datos para la tabla `padres`
---
 
-INSERT INTO `padres` (`nino`, `padre`, `key`, `pass`) VALUES
-(1, '75572325Y', '00:1B:44:11:3A:B7', '5baa61e4c9b93f3f0682250b6cf8331b7ee68fd8');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
