9b395fe5cfbfa018299c9f29f436798088cf911f
Benito Palacios Sánchez <benito356@gmail.com>
2014-10-29 01:15:42 +0100

    Movido centro infantil a carpeta estable

--- CentroInfantil/run.sh
+++ CentroInfantil/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd build/classes
-cp ../../../../rti_license.dat ./
+cp ../../../rti_license.dat ./
 cp ../../src/centroinfantil/USER_QOS_PROFILES.xml ./
 
 if [ $IS_RASPBERRY_PI ]; then
@@ -14,4 +14,4 @@ fi
 source $RTI_CONNEXT_PATH/rti_set_bash_5.1.0
 export PATH=$RTI_CONNEXT_PATH/openssl-1.0.1g/$RTI_ARCH_WAN/bin:$PATH
 export LD_LIBRARY_PATH=$NDDSHOME/lib/$RTI_ARCH:$NDDSHOME/lib/$RTI_ARCH_WAN:$RTI_CONNEXT_PATH/openssl-1.0.1g/$RTI_ARCH_WAN/lib:$LD_LIBRARY_PATH
-java -cp ../../../../Libs/gstreamer-java/gstreamer-java-1.6.jar:../../../../Libs/JNA/jna-4.1.0.jar:../../../../Libs/JNA/jna-platform-4.1.0.jar:../../../../DDStheus/dist/DDStheus.jar:$NDDSHOME/class/nddsjavad.jar:./ centroinfantil.CentroInfantil $@
+java -cp ../../../Libs/gstreamer-java/gstreamer-java-1.6.jar:../../../Libs/JNA/jna-4.1.0.jar:../../../Libs/JNA/jna-platform-4.1.0.jar:../../../DDStheus/dist/DDStheus.jar:$NDDSHOME/class/nddsjavad.jar:./ centroinfantil.CentroInfantil $@
